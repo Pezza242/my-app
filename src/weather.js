2b78d708f8f8c50e3e4e33a156e2547a3923ab37
Pezza242 <periyearwood@gmail.com>
2026-01-01 21:17:51 +0000

    Added function to weather.js
    Added function to the search bar and made link to change temperature from celsius to farenheit.

--- src/weather.js
+++ src/weather.js
@@ -1,21 +1,48 @@
+import { useState } from "react";
 import "./weather.css";
 import Animation from "./animation";
 
-export default function Weather() {
+export default function Weather(props) {
+  let [temperature, setTemperature] = useState(18);
+  let [city, setCity] = useState("London");
+
+  function Farenheit(event) {
+    event.preventDefault();
+    let farenheit = Math.round((temperature * 9) / 5 + 32);
+    return setTemperature(farenheit);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    return city;
+  }
   return (
     <div className="weather">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <input
           type="search"
           className="search-bar"
           placeholder="Enter a city...."
+          onChange={updateCity}
           required
         />
         <input type="submit" className="submit" />
       </form>
       <hr />
-      <h1 className="city">London</h1>
-      <h2 className="current-temperature">18ºC</h2>
+      <h1 className="city">{city}</h1>
+      <h2 className="current-temperature">
+        {temperature}
+        <a className="Celsius" href="/" onClick={temperature}>
+          ºC
+        </a>
+        |{" "}
+        <a className="Farenheit" href="/" onClick={Farenheit}>
+          ºF
+        </a>
+      </h2>
       <div className="icon">
         <Animation />
         <p className="icon-description">Sunny</p>
