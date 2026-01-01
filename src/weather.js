ddd671b54e6aa6dbeb570b5809437098203748fa
Pezza242 <periyearwood@gmail.com>
2026-01-01 18:07:58 +0000

    Created weather.js
    Pasted the code from my previous project I deleted onto this new project

---
+++ src/weather.js
@@ -0,0 +1,22 @@
+import "./weather.css";
+import Animation from "./animation";
+
+export default function Weather() {
+  return (
+    <div className="weather">
+      <form className="form">
+        <input
+          type="search"
+          className="search-bar"
+          placeholder="Enter a city...."
+          required
+        />
+        <input type="submit" className="submit" />
+      </form>
+      <hr />
+      <h1 className="city">London</h1>
+      <h2 className="current-temperature">18ºC</h2>
+      <Animation />
+    </div>
+  );
+}
